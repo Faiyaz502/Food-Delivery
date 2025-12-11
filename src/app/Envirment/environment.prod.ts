a334a14f0c4bc4a18d434cf6ab3d28e3dbaf273c
Faiyaz502 <www.fahim.faizas@gmail.com>
2025-12-11 13:17:07 +0600

    DSS

--- src/app/Envirment/environment.prod.ts
+++ src/app/Envirment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://food-delivery-backend-vqkb.onrender.com/api',
+  apiUrl: 'https://food-delivery-backend-vqkb.onrender.com',
   wsUrl: 'wss://food-delivery-backend-vqkb.onrender.com/ws-chat',
     endpoints: {
     orders: '/api/orders',
