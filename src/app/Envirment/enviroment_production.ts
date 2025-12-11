03df3b64fc51b86ca1a82cf5c7182bac2aa35834
Faiyaz502 <www.fahim.faizas@gmail.com>
2025-12-11 10:18:27 +0600

    dev

---
+++ src/app/Envirment/enviroment_production.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://food-delivery-backend-vqkb.onrender.com/api',
+  wsUrl: 'wss://food-delivery-backend-vqkb.onrender.com/ws-chat',
+    endpoints: {
+    orders: '/api/orders',
+    carts: '/api/carts'
+  }
+};
