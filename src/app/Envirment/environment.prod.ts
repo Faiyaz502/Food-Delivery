18f8b09dc90c0499b86bb46840a0bd0ba1f3553a
Faiyaz502 <www.fahim.faizas@gmail.com>
2025-12-14 10:24:01 +0600

    webs

--- src/app/Envirment/environment.prod.ts
+++ src/app/Envirment/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://food-delivery-backend-vqkb.onrender.com',
-  wsUrl: 'wss://food-delivery-backend-vqkb.onrender.com/ws-chat',
+  wsUrl: 'https://food-delivery-backend-vqkb.onrender.com/ws-chat',
     endpoints: {
     orders: '/api/orders',
     carts: '/api/carts'
--- src/app/Envirment/environment.ts
+++ src/app/Envirment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 production: false,
   apiUrl: 'http://localhost:8080',
-  wsUrl: 'ws://localhost:8080/ws-chat',
+  wsUrl: 'http://localhost:8080/ws-chat',
   userId:2, // home 2 tsp 3
   ownerId:3, // tsp4 , home 3
   riderId : 4, // home 4 , tsp 1
