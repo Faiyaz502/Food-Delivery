021a4053491c65b67e701f5eda50fb2a055ffa9d
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-01-13 13:57:09 +0600

    tokenRefresh

--- src/app/unauthorized/unauthorized.component.ts
+++ src/app/unauthorized/unauthorized.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from '../services/authService/auth-service.service';
 
 @Component({
   selector: 'app-unauthorized',
   templateUrl: './unauthorized.component.html',
   styleUrls: ['./unauthorized.component.scss']
 })
-export class UnauthorizedComponent {
+export class UnauthorizedComponent implements OnInit {
+
+
+  constructor(private au : AuthServiceService){}
+
+
+  ngOnInit(): void {
+
+    this.au.logout();
+
+
+
+  }
+
+
+
+
+
+
+
 
 }
